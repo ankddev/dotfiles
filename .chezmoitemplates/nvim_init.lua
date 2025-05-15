ea369a2c199d96ad1a62d8120225a6c05598204c
ANKDDEV <andrekabatareika@gmail.com>
2025-05-15 06:58:41 +0300

    fix: wrong templates

--- .chezmoitemplates/nvim_init.lua
+++ .chezmoitemplates/nvim_init.lua
@@ -1,5 +1,9 @@
 -- Config
 require('core.config')
+-- Neovide specific configuration
+if vim.g.neovide then
+    require('core.neovide')
+end
 require('core.tabs')
 require('core.search')
 -- Plugins
