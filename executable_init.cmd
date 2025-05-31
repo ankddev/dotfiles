50c8e074558cb66357ee92284cf50ea1ea7b9a1d
ANKDDEV <andrekabatareika@gmail.com>
2025-05-31 13:02:39 +0300

    (init.cmd): add `--include-unknown` flag to WinGet when updating packages

--- executable_init.cmd
+++ executable_init.cmd
@@ -1,6 +1,6 @@
 @echo off
 doskey alias=if ".$*." == ".." ( doskey /MACROS ) else ( doskey $* )
 doskey ls=dir /b $*
-doskey update=cmd /S /C "echo Updating Rust && rustup update && echo Updating Cargo packages && cargo install-update --all && echo Updating Scoop packages && scoop update * && echo Updating Winget packages && winget upgrade --all && echo Updating Go packages && go-global-update && echo Updated everything!"
+doskey update=cmd /S /C "echo Updating Rust && rustup update && echo Updating Cargo packages && cargo install-update --all && echo Updating Scoop packages && scoop update * && echo Updating Winget packages && winget upgrade --all --include-unknown && echo Updating Go packages && go-global-update && echo Updated everything!"
 doskey diskspace=powershell "Get-PSDrive -PSProvider FileSystem | Select-Object Name, @{Name='Used(GB)';Expression={($_.Used/1GB).ToString('F2')}}, @{Name='Free(GB)';Expression={($_.Free/1GB).ToString('F2')}}, @{Name='Total(GB)';Expression={($_.Used + $_.Free)/1GB}}"
 
