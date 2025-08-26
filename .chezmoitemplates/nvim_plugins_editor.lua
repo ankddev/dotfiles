b65faf890b93b177ca4c863a186c52e63313eb27
ANKDDEV <190183925+ankddev@users.noreply.github.com>
2025-08-26 13:27:07 +0300

    nvim: add outline plugin
    Signed-off-by: ANKDDEV <190183925+ankddev@users.noreply.github.com>

--- .chezmoitemplates/nvim_plugins_editor.lua
+++ .chezmoitemplates/nvim_plugins_editor.lua
@@ -69,4 +69,13 @@ return {
 		},
 	},
 	{ "wakatime/vim-wakatime", lazy = false },
+	{
+		"hedyhli/outline.nvim",
+		lazy = true,
+		cmd = { "Outline", "OutlineOpen" },
+		keys = {
+			{ "<leader>o", "<cmd>Outline<CR>", desc = "Toggle outline" },
+		},
+		opts = {},
+	},
 }
